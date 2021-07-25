f8e12d8ef5610a38c621ec4a4ac059d40fedbbf9
Nano <marianodilella86@gmail.com>
2021-07-25 11:53:54 -0300

    commit 2

---
+++ desafio.js
@@ -0,0 +1,36 @@
+const categoriaInput = document.getElementById("categoria");
+const descripcionInput = document.getElementById("descripcion");
+const contenidoTabla = document.getElementById("contenidoTabla");
+
+
+const notas = [];
+
+function agregarNota(event) {
+  event.preventDefault();
+  const categoria = categoriaInput.value;
+  const descripcion = descripcionInput.value;
+
+  const nuevaNota = { categoria: categoria, descripcion: descripcion };
+
+  notas.push(nuevaNota);
+  event.target.reset();
+  mostrarNotas();
+}
+
+function mostrarNotas() {
+const contenido = notas.map(function (nota) {
+    const tr = `
+    <tr>
+                <th scope="row">${nota.categoria}</th>
+                <td>${nota.descripcion}</td>
+                <td><button class="btn btn-warning">Editar</button></td>
+                
+    </tr> 
+    `;
+    return tr;
+});
+
+contenidoTabla.innerHTML = contenido.join('');
+
+
+}
