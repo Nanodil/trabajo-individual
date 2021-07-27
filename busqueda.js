3ba5bfdcf360c2663a1c759c8f6c38dbcfc49d47
Nano <marianodilella86@gmail.com>
2021-07-27 20:26:29 -0300

    Commit agregado de busqueda

---
+++ busqueda.js
@@ -0,0 +1,12 @@
+function buscarnotas(event) {
+    event.preventDefault();
+    const busquedaInput = document.getElementById('busqueda');
+    console.log(busquedaInput.value);
+    function notasFilter(notas) {
+        return notas.categoria.includes(busquedaInput.value);
+
+    }
+    const notasFiltrados = notas.filter(notasFilter);
+    notas = notasFiltrados
+    mostrarNotas()
+}
